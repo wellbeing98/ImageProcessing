3a5acc58a260ca686198700094eb3216a33bc1da
wellbeing98 <tom9816@gmail.com>
2023-03-28 17:45:24 +0900

    commit 3

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ src = cv2.imread("images/picture01.jpg")
 
 ## 영상 처리 알고리즘 구현
 dst1 = cv2.cvtColor(src,cv2.COLOR_RGB2GRAY)
-
+dst2 = src.copy()
+_, dst2  = cv2.threshold(dst2, 127, 255, cv2.THRESH_BINARY)
 
 # 영상 디스플레이
 cv2.imshow('src', src)
