1476bc16118a2eb14c5c241b8e7251dfe06d0a5a
wellbeing98 <tom9816@gmail.com>
2023-03-28 17:41:53 +0900

    commti2

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ import cv2
 
 src = cv2.imread("images/picture01.jpg")
 
+## 영상 처리 알고리즘 구현
+dst1 = cv2.cvtColor(src,cv2.COLOR_RGB2GRAY)
 
 
 # 영상 디스플레이
 cv2.imshow('src', src)
-
+cv2.imshow('dst1', dst1)
 
 # 마무리
 
 
 cv2.waitKey(0)
-cv2.destoryAllWindows()
+cv2.destroyAllWindows()
